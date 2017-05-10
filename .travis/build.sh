bc5f22657915745724488a8e7a3364d16facc2e9
Phus Lu <phuslu@hotmail.com>
2017-05-10 13:08:49 +0800

    add files
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 set -ex
+
+pushd /tmp
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.1.0/git-lfs-linux-amd64-2.1.0.tar.gz
+tar xvf git-lfs-linux-amd64-2.1.0.tar.gz
+export PATH=$PATH:$(pwd)/git-lfs-2.1.0/
+popd
+
 rootdir=$(git rev-parse --show-toplevel)
 cd ${rootdir}
+git lfs update
 find . -type d | grep -vE '^(\./\.)' | xargs -n1 -i python .travis/gen_index_html.py {}
 find . -type f -name "index.html" -exec git add "{}" \;
 
@@ -14,4 +22,11 @@ git checkout -b master
 git commit -m "[skip ci]: gen index.html" -s -a
 set +ex
 git push https://phuslu:${GITHUB_TOKEN}@github.com/phuslu/phuslu.github.io.git master
-test -n "$MIRROR_REMOTE_URL" && git push "$MIRROR_REMOTE_URL" master -f
+
+if test -n "$MIRROR_REMOTE_URL" ; then
+    rm -rf .git*
+    git init
+    git add --all .
+    git commit -m "import github pages" -s -a
+    git push "$MIRROR_REMOTE_URL" master -f
+fi
---
+++ goproxy/README.md
@@ -0,0 +1,6 @@
+尊敬的网友们好：
+
+1. 下载 goproxy 并用管理员权限运行
+1. Windows 用户推荐使用 goproxy-gui.exe 托盘图标设置 IE 代理(对其它浏览器也有效)
+1. Chrome/Opera 请安装 [SwitchyOmega](SwitchyOmega.crx) 插件(下载插件到本地然后拖放到 `chrome://extensions/` 页面上)
+1. Firefox 请安装 [FoxyProxy](https://addons.mozilla.org/en-US/firefox/addon/foxyproxy-standard/) ，Firefox 需要导入证书，方法请见 FAQ
