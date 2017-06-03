ceb1d2117f8e432dc8a233b2dedc0d68edf90665
Phus Lu <phuslu@hotmail.com>
2017-06-03 12:08:16 +0800

    simplify
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- candidate/remote_node_exporter.py
+++ candidate/remote_node_exporter.py
@@ -4,15 +4,6 @@
 #     MIT License, Copyright phuslu@hotmail.com
 # Usage:
 #     /usr/bin/env DAEMON=1 PORT=9101 SSH_HOST=192.168.2.1 SSH_USER=admin SSH_PASS=123456 /home/phuslu/phuslu.github.io/candidate/remote_node_exporter.py
-# TODO:
-#     /proc/diskstats
-#     /sys/class/hwmon
-#     process_cpu_seconds_total
-#     process_max_fds
-#     process_open_fds
-#     process_resident_memory_bytes
-#     process_start_time_seconds
-#     process_virtual_memory_bytes
 
 
 import BaseHTTPServer
@@ -37,6 +28,7 @@ ENV_PORT = os.environ.get('PORT')
 
 ssh_client = None
 this_metric = ''
+this_text = ''
 
 libc = ctypes.CDLL(ctypes.util.find_library('c'))
 
@@ -108,21 +100,21 @@ def read_file(filename):
 
 
 def print_metric_type(metric, mtype):
-    global this_metric
+    global this_metric, this_text
     this_metric = metric
-    return '# TYPE %s %s\n' % (metric, mtype)
+    this_text += '# TYPE %s %s\n' % (metric, mtype)
 
 
 def print_metric(labels, value):
+    global this_metric, this_text
     if isinstance(value, float):
         value = '%e' % value if value else '0'
     else:
         value = str(value)
     if labels:
-        s = '%s{%s} %s\n' % (this_metric, labels, value)
+        this_text += '%s{%s} %s\n' % (this_metric, labels, value)
     else:
-        s = '%s %s\n' % (this_metric, value)
-    return s
+        this_text += '%s %s\n' % (this_metric, value)
 
 
 def collect_time():
@@ -137,109 +129,93 @@ def collect_time():
         ts = float(system_time)
     else:
         ts = float(do_exec_command('date +%s').strip() or '0')
-    s = ''
-    s += print_metric_type('node_time', 'counter')
-    s += print_metric(None, ts)
-    return s
+    print_metric_type('node_time', 'counter')
+    print_metric(None, ts)
 
 
 def collect_loadavg():
     loadavg = read_file('/proc/loadavg').strip().split()
-    s = ''
-    s += print_metric_type('node_load1', 'gauge')
-    s += print_metric(None, float(loadavg[0]))
-    s += print_metric_type('node_load15', 'gauge')
-    s += print_metric(None, float(loadavg[2]))
-    s += print_metric_type('node_load5', 'gauge')
-    s += print_metric(None, float(loadavg[1]))
-    return s
+    print_metric_type('node_load1', 'gauge')
+    print_metric(None, float(loadavg[0]))
+    print_metric_type('node_load15', 'gauge')
+    print_metric(None, float(loadavg[2]))
+    print_metric_type('node_load5', 'gauge')
+    print_metric(None, float(loadavg[1]))
 
 
 def collect_filefd():
     file_nr = read_file('/proc/sys/fs/file-nr').split()
-    s = ''
-    s += print_metric_type('node_filefd_allocated', 'gauge')
-    s += print_metric(None, float(file_nr[0]))
-    s += print_metric_type('node_filefd_maximum', 'gauge')
-    s += print_metric(None, float(file_nr[2]))
-    return s
+    print_metric_type('node_filefd_allocated', 'gauge')
+    print_metric(None, float(file_nr[0]))
+    print_metric_type('node_filefd_maximum', 'gauge')
+    print_metric(None, float(file_nr[2]))
 
 
 def collect_nf_conntrack():
     nf_conntrack_count = read_file('/proc/sys/net/netfilter/nf_conntrack_count').strip()
     nf_conntrack_max = read_file('/proc/sys/net/netfilter/nf_conntrack_max').strip()
-    s = ''
     if nf_conntrack_count:
-        s += print_metric_type('node_nf_conntrack_entries', 'gauge')
-        s += print_metric(None, float(nf_conntrack_count))
+        print_metric_type('node_nf_conntrack_entries', 'gauge')
+        print_metric(None, float(nf_conntrack_count))
     if nf_conntrack_max:
-        s += print_metric_type('node_nf_conntrack_entries_limit', 'gauge')
-        s += print_metric(None, float(nf_conntrack_max))
-    return s
+        print_metric_type('node_nf_conntrack_entries_limit', 'gauge')
+        print_metric(None, float(nf_conntrack_max))
 
 
 def collect_memory():
     meminfo = read_file('/proc/meminfo')
     meminfo = meminfo.replace(')', '').replace(':', '').replace('(', '_')
     meminfo = meminfo.splitlines()
-    s = ''
     for mi in meminfo:
         mia = mi.split()
         print_metric_type('node_memory_%s' % mia[0], 'gauge')
         if len(mia) == 3:
-            s += print_metric(None, float(int(mia[1]) * 1024))
+            print_metric(None, float(int(mia[1]) * 1024))
         else:
-            s += print_metric(None, float(mia[1]))
-    return s
+            print_metric(None, float(mia[1]))
 
 
 def collect_netstat():
     netstat = (read_file('/proc/net/netstat') + read_file('/proc/net/snmp')).splitlines()
-    s = ''
     for i in range(0, len(netstat), 2):
         prefix, keystr = netstat[i].split(': ', 1)
         prefix, valuestr = netstat[i+1].split(': ', 1)
         keys = keystr.split()
         values = valuestr.split()
         for ii, ss in enumerate(keys):
-            s += print_metric_type('node_netstat_%s_%s' % (prefix, ss), 'gauge')
-            s += print_metric(None, float(values[ii]))
-    return s
+            print_metric_type('node_netstat_%s_%s' % (prefix, ss), 'gauge')
+            print_metric(None, float(values[ii]))
 
 
 def collect_vmstat():
     vmstat = read_file('/proc/vmstat').splitlines()
-    s = ''
     for vm in vmstat:
         vma = vm.split()
-        s += print_metric_type('node_vmstat_%s' % vma[0], 'gauge')
-        s += print_metric(None, float(vma[1]))
-    return s
+        print_metric_type('node_vmstat_%s' % vma[0], 'gauge')
+        print_metric(None, float(vma[1]))
 
 
 def collect_stat():
     cpu_mode = 'user nice system idle iowait irq softirq steal guest guest_nice'.split()
     stat = read_file('/proc/stat')
-    s = ''
-    s += print_metric_type('node_boot_time', 'gauge')
-    s += print_metric(None, float(re.search(r'btime ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_context_switches', 'counter')
-    s += print_metric(None, float(re.search(r'ctxt ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_forks', 'counter')
-    s += print_metric(None, float(re.search(r'processes ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_intr', 'counter')
-    s += print_metric(None, float(re.search(r'intr ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_procs_blocked', 'gauge')
-    s += print_metric(None, float(re.search(r'procs_blocked ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_procs_running', 'gauge')
-    s += print_metric(None, float(re.search(r'procs_running ([0-9]+)', stat).group(1)))
-    s += print_metric_type('node_cpu', 'counter')
+    print_metric_type('node_boot_time', 'gauge')
+    print_metric(None, float(re.search(r'btime ([0-9]+)', stat).group(1)))
+    print_metric_type('node_context_switches', 'counter')
+    print_metric(None, float(re.search(r'ctxt ([0-9]+)', stat).group(1)))
+    print_metric_type('node_forks', 'counter')
+    print_metric(None, float(re.search(r'processes ([0-9]+)', stat).group(1)))
+    print_metric_type('node_intr', 'counter')
+    print_metric(None, float(re.search(r'intr ([0-9]+)', stat).group(1)))
+    print_metric_type('node_procs_blocked', 'gauge')
+    print_metric(None, float(re.search(r'procs_blocked ([0-9]+)', stat).group(1)))
+    print_metric_type('node_procs_running', 'gauge')
+    print_metric(None, float(re.search(r'procs_running ([0-9]+)', stat).group(1)))
+    print_metric_type('node_cpu', 'counter')
     cpulines = re.findall(r'(?m)cpu\d+ .+', stat)
     for i, line in enumerate(cpulines):
         cpu = line.split()[1:]
         for ii, mode in enumerate(cpu_mode):
-            s += print_metric('cpu="cpu%d",mode="%s"' % (i, mode), float(cpu[ii]) / 100)
-    return s
+            print_metric('cpu="cpu%d",mode="%s"' % (i, mode), float(cpu[ii]) / 100)
 
 
 def collect_netdev():
@@ -252,13 +228,11 @@ def collect_netdev():
         if len(stats) > 0:
             devices.append(stats[0])
             statss.append(stats[1:])
-    s = ''
     for i in range(len(statss[0])):
         inter = 'receive' if 2*i < len(statss[0]) else 'transmit'
-        s += print_metric_type('node_network_%s_%s' % (inter, faces[i]), 'gauge')
+        print_metric_type('node_network_%s_%s' % (inter, faces[i]), 'gauge')
         for ii, value in enumerate(devices):
-            s += print_metric('device="%s"' % value, float(statss[ii][i]))
-    return s
+            print_metric('device="%s"' % value, float(statss[ii][i]))
 
 
 def collect_diskstats():
@@ -281,12 +255,10 @@ def collect_diskstats():
         values = line.split()[2:14]
         device = values.pop(0)
         devices[device] = values
-    s = ''
     for i, suffix in enumerate(suffixs):
-        s += print_metric_type('node_disk_%s' % suffix, 'gauge')
+        print_metric_type('node_disk_%s' % suffix, 'gauge')
         for device, values in devices.items():
-            s += print_metric('device="%s"' % device, float(values[i]))
-    return s
+            print_metric('device="%s"' % device, float(values[i]))
 
 
 def collect_filesystem():
@@ -296,7 +268,7 @@ def collect_filesystem():
     mountinfo = read_file('/proc/mounts').strip().splitlines()
     df = do_exec_command('df').splitlines()
     if not df:
-        return ''
+        return
     df.pop(0)
     mountpoints = {}
     for line in mountinfo:
@@ -310,31 +282,30 @@ def collect_filesystem():
         device, size, used, avail, _, mountpoint = line.split()[:6]
         if mountpoint in mountpoints:
             mountpoints[mountpoint].update(dict(size=int(size)*1024, free=(int(size)-int(used))*1024, avail=int(avail)*1024))
-    s = ''
     for suffix in suffixs:
-        s += print_metric_type('node_filesystem_%s' % suffix, 'gauge')
+        print_metric_type('node_filesystem_%s' % suffix, 'gauge')
         for mountpoint, info in mountpoints.items():
-            s += print_metric('device="%(device)s",fstype="%(fstype)s",mountpoint="%(mountpoint)s"' % info, float(info.get(suffix, 0)))
-    return s
+            print_metric('device="%(device)s",fstype="%(fstype)s",mountpoint="%(mountpoint)s"' % info, float(info.get(suffix, 0)))
 
 
 def collect_all():
+    global this_text
+    this_text = ''
     if ssh_client is None:
         do_connect()
     do_preread()
-    s = ''
-    s += collect_time()
-    s += collect_loadavg()
-    s += collect_stat()
-    s += collect_vmstat()
-    s += collect_memory()
-    s += collect_filefd()
-    s += collect_nf_conntrack()
-    s += collect_netstat()
-    s += collect_netdev()
-    s += collect_diskstats()
-    # s += collect_filesystem()
-    return s
+    collect_time()
+    collect_loadavg()
+    collect_stat()
+    collect_vmstat()
+    collect_memory()
+    collect_filefd()
+    collect_nf_conntrack()
+    collect_netstat()
+    collect_netdev()
+    collect_diskstats()
+    # collect_filesystem()
+    return this_text
 
 
 class MetricsHandler(BaseHTTPServer.BaseHTTPRequestHandler):
